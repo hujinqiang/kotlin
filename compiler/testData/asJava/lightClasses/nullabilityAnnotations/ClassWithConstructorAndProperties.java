f7de08b764997cf714a1906fb81d258880115e96
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2020-05-20 13:49:24 +0300

    TESTS

--- compiler/testData/asJava/lightClasses/nullabilityAnnotations/ClassWithConstructorAndProperties.java
+++ compiler/testData/asJava/lightClasses/nullabilityAnnotations/ClassWithConstructorAndProperties.java
@@ -1,12 +1,25 @@
+<<<<<<< HEAD
 public final class ClassWithConstructorAndProperties {
     private final java.lang.String nullable;
     private final java.lang.String notNull;
+=======
+public final class ClassWithConstructorAndProperties /* ClassWithConstructorAndProperties*/ {
+  private final java.lang.String notNull;
+>>>>>>> TESTS
 
-    @org.jetbrains.annotations.Nullable
-    public final java.lang.String getNullable() { /* compiled code */ }
+  private final java.lang.String nullable;
 
-    @org.jetbrains.annotations.NotNull
-    public final java.lang.String getNotNull() { /* compiled code */ }
+  @null()
+  public  ClassWithConstructorAndProperties(@org.jetbrains.annotations.Nullable() java.lang.String, @org.jetbrains.annotations.NotNull() java.lang.String);//  .ctor(java.lang.String, java.lang.String)
 
+<<<<<<< HEAD
     public ClassWithConstructorAndProperties(@org.jetbrains.annotations.Nullable java.lang.String nullable, @org.jetbrains.annotations.NotNull java.lang.String notNull) { /* compiled code */ }
+=======
+  @org.jetbrains.annotations.NotNull()
+  public final java.lang.String getNotNull();//  getNotNull()
+
+  @org.jetbrains.annotations.Nullable()
+  public final java.lang.String getNullable();//  getNullable()
+
+>>>>>>> TESTS
 }
